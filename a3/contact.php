f378f875af7c1bc45ff9cb9dca50282110685c18
Quinn0809 <s3724287@student.rmit.edu.au>
2020-09-01 06:41:03 +1000

    Update contact.php

--- a3/contact.php
+++ a3/contact.php
@@ -35,7 +35,7 @@
     </fieldset>
     
     <fieldset>
-    <input type="checkbox" name="remember" value="<?= $remember ?>" class="remember">  <span class="remember_span">Remember me</span> 
+    <input type="checkbox" name="remember" value="1" class="remember">  <span class="remember_span">Remember me</span> 
     </fieldset>
 
     <fieldset>
