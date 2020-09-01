ac1c7b35b72495cb506bfc759436e8e56f29bcc9
Quinn0809 <s3724287@student.rmit.edu.au>
2020-09-01 13:32:36 +1000

    Update postcards.php

--- a3/postcards.php
+++ a3/postcards.php
@@ -4,9 +4,6 @@
      include 'header.php'; 
    ?>
 
-
-<section>
-
 <div class="centered-container-2">
 
 <h1> Letters </h1>
@@ -22,27 +19,6 @@
             </div> 
 </div>
 
-</section>
-
-<section>
-
-<div class="centered-container-2">
-
-<h1> Letters </h1>
-<div class="wrapper">
-                  <div class="mail">
-                     <div class="cover">
-                        <div class="letter">
-                           <h1>August 31st 1914</h1>
-                           <p>On Saturday when we came into camp from drill at 12 o’clock, we were told that the Commandant was very pleased with the progress made and had granted us a half holiday so the Gympie lads decided to go to the city. We had a good time in Brisbane and had tea at a fish shop.  Met Herb at about 7 p.m. and we all went to the Empire but the show was very poor. We have had issued to us two towels, two pairs under-pants, two singlets, two pairs socks, a sweater and a cake of soap. I think we will get boots in a day or two and other things.
-               </br>A church parade was held but none of the Gympie lads attended. It happened to be a voluntary one! We are all keeping in pretty good health and hope you all are at home. Today is payday, we are to get paid every fortnight, this time we will get 11 days. I am only drawing half of mine and banking the rest. Love to all. </p>
-                        </div>
-                     </div>
-                  </div>
-            </div> 
-</div>
-
-</section>
 
 <?php 
      include 'footer.php'; 
